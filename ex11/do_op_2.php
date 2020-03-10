b205439dd64930c8b3a3e9c36453d8e58826b68b
Rasmus Jaakonmaki <rjaakonm@c1r5p14.hive.fi>
2020-03-10 14:39:57 +0200

    change do_op_2 to use regex and work with any values, like -002*-002

--- ex11/do_op_2.php
+++ ex11/do_op_2.php
@@ -3,38 +3,32 @@
 if ($argc == 2)
 {
 	$line = trim($argv[1]);
-	$arr = str_split($line);
-	foreach($arr as $char)
+	$line = preg_replace('/( |\t)+/', "", $line);
+	$pattern = '/([-+]?[0-9]+)([+\-*\/%])([-+]?[0-9]+)/';
+	preg_match_all($pattern, $line, $matches, PREG_SET_ORDER);
+	if ($matches[0][0] === $line && count($matches[0]) === 4)
 	{
-		if (in_array($char, array('+', '-', '*', '/', '%')))
-		{
-			$op = $char;
-			break ;
-		}
+		$op = $matches[0][2];
+		$nb1 = $matches[0][1];
+		$nb2 = $matches[0][3];
+		if (!is_numeric($nb1) || !is_numeric($nb2))
+			exit("Syntax Error\n");
+		if ($nb2 == 0 && ($op == "/" || $op == "%"))
+			exit("Division by 0 error\n");
+		if ($op == "+")
+			$nb = $nb1 + $nb2;
+		else if ($op == "-")
+			$nb = $nb1 - $nb2;
+		else if ($op == "*")
+			$nb = $nb1 * $nb2;
+		else if ($op == "/")
+			$nb = $nb1 / $nb2;
+		else if ($op == "%")
+			$nb = $nb1 % $nb2;
+		echo $nb."\n";
 	}
-	if (!isset($op))
+	else
 		exit("Syntax Error\n");
-	$line = implode("", $arr);
-	$arr = explode($op, $line);
-	if (count($arr) != 2)
-		exit("Syntax Error\n");
-	$nb1 = trim($arr[0]);
-	$nb2 = trim($arr[1]);
-	if (!is_numeric($nb1) || !is_numeric($nb2))
-		exit("Syntax Error\n");
-	if ($nb2 == 0 && ($op == "/" || $op == "%"))
-		exit("Division by 0 error\n");
-	if ($op == "+")
-		$nb = $nb1 + $nb2;
-	else if ($op == "-")
-		$nb = $nb1 - $nb2;
-	else if ($op == "*")
-		$nb = $nb1 * $nb2;
-	else if ($op == "/")
-		$nb = $nb1 / $nb2;
-	else if ($op == "%")
-		$nb = $nb1 % $nb2;
-	echo $nb."\n";
 }
 else
 	exit("Incorrect Parameters\n");
