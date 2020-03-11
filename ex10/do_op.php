565b10f7741f0d9ae6bb6194e9bf5961dbd1f44d
Rasmus Jaakonmaki <rjaakonm@c2r4p10.hive.fi>
2020-03-11 11:23:33 +0200

    removed extra echo

--- ex10/do_op.php
+++ ex10/do_op.php
@@ -4,7 +4,6 @@ if ($argc == 4)
 {
 	$nb1 = preg_replace('/( |\t)+/', '', $argv[1]);
 	$nb2 = preg_replace('/( |\t)+/', '', $argv[3]);
-	echo "$nb1 and $nb2\n";
 	$op = trim($argv[2]);
 	if ($op == "+")
 		$nb = $nb1 + $nb2;
